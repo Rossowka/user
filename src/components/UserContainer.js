845c2cf8d5618d2707b89f0cf8528b4ab547e995
Sara Rossow <rossowka@icloud.com>
2023-01-09 12:23:09 +0100

    handling endpoind error

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -7,7 +7,14 @@ function UserContainer() {
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users') // https://jsonplaceholder.typicode.com/users
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                throw new Error(
+                  `This is an HTTP error: The status is ${response.status}`
+                );
+              }
+              return response.json();
+            })
             .then((actualData) => console.log(actualData))
             .catch((err) => {
                 console.log(err.message);
